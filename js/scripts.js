6f75700d8237f18da2421d55cb45d14c70e14751
Tiffany Nguyen <tiffany.ngu713@gmail.com>
2020-01-24 11:42:14 -0800

    create an if statement for 1 but it does not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
-	$(form#result).submit(function(){
-		var number = $(#list).val();
-		console.log(number)
-		$("#result").hide();
-		result = "";
+$(document).ready(function() {
+	$(form#input).submit(function(){
+		var num = $(#num).val();
+		$("#list").hide();
+		result = " ";
+		var num
+		var beep = "Beep!"
+		var boop = "Boop!"
+		var dave = "I'm sorry, Dave. I'm afraid I can't do that."
+		
+		var number = ["0", "1", "2", "3", "4", "5"];
+		number.splice(1, num.length - 1, "Boop!");
+		console.log(number);
+// var total
+// 		for(i =0; i<= 9; i+= 1) {
+// 			if (number[i] === 1) {
+// 			result += var beep;
+// 		}
+// 	}
 
-		for()
+	// $(".list").text(result);
+
+	event.preventDefault();
+
+	});
+});
