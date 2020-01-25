a740228cd995559f8a68040712936f3fcd1730a4
Tiffany Nguyen <tiffany.ngu713@gmail.com>
2020-01-24 16:06:25 -0800

    add splice method but the code doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,44 @@
 $(document).ready(function() {
 	$("form#input").submit(function(event) {
-		
+		var userInput = $("#userInput").val();
 	
-		var result = [];
+		userInput.splice("userResult","userInput","Boop!")
 		
+		// console.log(number);
+
+		result = [];
+
+		// for(i=0; i<= userInput; i+= 1) {
+		// 	if (userInput[i] === "1") {
+		// 		result += "Boop!";
+		// } else if (userInput[i] === "2") {
+		// 		result += "Beep!";
+		// } else if (userInput[i] === "3") {
+		// 		result += "I'm sorry, Dave. I'm afraid I can't do that.";	
+		// } else { result += userInput[i]; 
+		
+		// 	$(".userResult").text(result);
 
 		for(i=0; i<= userInput; i+= 1) {
 			if (i.toString().includes("1")) {
-			result.push("Beep!");
+			result.replace("Beep!");
 			} else if (i.toString().includes("2")) {
-			result.push("Boop!");
+			result.replace("Boop!");
 			} else if (i.toString().includes("3")) {
-			result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+			result.replace("I'm sorry, Dave. I'm afraid I can't do that.");
 			} 
 			else {
 			result.push(" " +i);
 			}
 		}
 		return result;
+		
+		console.log(number);
 
 		event.preventDefault();
 		
 	});
+	
 
 
 });
