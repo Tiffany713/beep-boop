c0c8397a78a29a507269ec6a309afdb5d9618716
Student <example@example.com>
2020-01-30 17:33:02 -0800

    define 'results' as a variable and add parseInt to userInput

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
+
+
 $(document).ready(function() {
-	$("form#input").submit(function(event) {
+	$("#input").submit(function(event) {
 		event.preventDefault();
-		var userInput = $("#userInput").val();
+		var userInput = parseInt($("#userInput").val());
+		$('#results').text(beepBoop(userInput));
 
-		var result = [];
-
-		
-
-		for(i=0; i<= userInput; i++) {
-			if (i.toString().includes("3")) {
-			result.push("I'm sorry, Dave. I'm afraid I can't do that.");
-			} else if (i.toString().includes("2")) {
-			result.push("Boop!");
-			} else if (i.toString().includes("1")) {
-			result.push("Beep!");
-			} 
-			else {
-			result.push(" " +i);
-			}
-		}
-		return result;
-
-
-		event.preventDefault();
-		
 	});
-	
-
-
 });
+
+
+function beepBoop(userInput) {
+	
+	var results = [];
+	
+	for(var i = 0; i <= userInput; i++) {
+		if (i.toString().includes("3")) {
+			results.push("I'm sorry, Dave. I'm afraid I can't do that.");
+		} else if (i.toString().includes("2")) {
+			results.push("Boop!");
+		} else if (i.toString().includes("1")) {
+			results.push("Beep!");
+		} else {
+			results.push(i);
+		}
+	}
+	return results;
+}
+
+
+
