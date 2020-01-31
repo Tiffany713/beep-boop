589a58cff55e4f042498550ba7357157bd38be76
Student <example@example.com>
2020-01-30 17:09:42 -0800

    reverse the order of the else if statements in order of importance

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,19 @@
 $(document).ready(function() {
 	$("form#input").submit(function(event) {
+		event.preventDefault();
 		var userInput = $("#userInput").val();
-	
-		userInput.splice("userResult","userInput","Boop!")
+
+		var result = [];
+
 		
-		// console.log(number);
 
-		result = [];
-
-		// for(i=0; i<= userInput; i+= 1) {
-		// 	if (userInput[i] === "1") {
-		// 		result += "Boop!";
-		// } else if (userInput[i] === "2") {
-		// 		result += "Beep!";
-		// } else if (userInput[i] === "3") {
-		// 		result += "I'm sorry, Dave. I'm afraid I can't do that.";	
-		// } else { result += userInput[i]; 
-		
-		// 	$(".userResult").text(result);
-
-		for(i=0; i<= userInput; i+= 1) {
-			if (i.toString().includes("1")) {
-			result.push("Beep!");
+		for(i=0; i<= userInput; i++) {
+			if (i.toString().includes("3")) {
+			result.push("I'm sorry, Dave. I'm afraid I can't do that.");
 			} else if (i.toString().includes("2")) {
 			result.push("Boop!");
-			} else if (i.toString().includes("3")) {
-			result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+			} else if (i.toString().includes("1")) {
+			result.push("Beep!");
 			} 
 			else {
 			result.push(" " +i);
